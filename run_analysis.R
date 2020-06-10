b208345da806dbe5c2403bbd116adefa7b4c0088
João Pedro <49729323+jpspm@users.noreply.github.com>
2020-06-10 15:23:35 -0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,44 +1,29 @@
 library(dplyr)
 
-#read resource data
 
-#train data
-traineX <- read.table("./UCI HAR Dataset/train/X_train.txt")
-traineY <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-subjectTrain<- read.table("./UCI HAR Dataset/train/subject_train.txt")
+TrainX <- read.table("./UCI HAR Dataset/train/X_train.txt")
+TrainY <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+TestX <- read.table("./UCI HAR Dataset/test/X_test.txt")
+TestY <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+varNames <- read.table("./UCI HAR Dataset/features.txt")
+actLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-#test data
-testX <- read.table("./UCI HAR Dataset/test/X_test.txt")
-testY <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+mergedX <- rbind(TrainX, TestX)
+mergedY <- rbind(TrainY, TestY)
+Sub_total <- rbind(Sub_train, Sub_test)
 
-#data description
-var_names<- read.table("./UCI HAR Dataset/features.txt")
+selectVar <- varNames[grep("mean\\(\\)|std\\(\\)",varNames[,2]),]
+mergedX <- mergedX[,selectVar[,1]]
 
-#activity labels
-act_labels<- read.table("./UCI HAR Dataset/activity_labels.txt")
-
-#1- merging the test and train data
-mergedX<- rbind(traineX, testX)
-mergedY<- rbind(traineY, testY)
-mergedSub<- rbind(subjectTrain, subjectTest)
-
-#2- Extracts only the measurements on the mean and standard deviation for each measurement.
-
-refineData<- var_names[grep("mean\\(\\) | std\\(\\)", var_names[,2]),]
-mergedX <- mergedX[,refineData[,1]]
-
-#3- use descriptive activity name in the data set
 colnames(mergedY) <- "activity"
-mergedY$activitylabel <- factor(mergedY$activity, labels = as.character(act_labels[,2]))
-activitylabel<- mergedY[,-1]
+mergedY$activitylabel <- factor(mergedY$activity, labels = as.character(actLabels[,2]))
+activitylabel <- mergedY[,-1]
 
-#4- Appropriately labels the data set with descriptive variable names
-colnames(mergedX)<- var_names[refineData[,1],2]
+colnames(mergedX) <- varNames[selectVar[,1],2]
 
-#5- using the dataset int step 4, create a second, independent
-#tidy data set with theaverage of each variable for each activity and each subject
-colnames(mergedSub)<- "subject"
-total<- cbind(mergedX, activitylabel, mergedSub)
-totalMean<- total %>% group_by(activitylabel, subject)%>% summarize_each(list(~mean))
-write.table(totalMean, file = "./UCI HAR Dataset/tidydata.txt", row.names = F, col.names = T)
+colnames(Sub_total) <- "subject"
+total <- cbind(mergedX, activitylabel, Sub_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./UCI HAR Dataset/finalData.txt", row.names = FALSE, col.names = TRUE)
